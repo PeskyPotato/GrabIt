619aa2742325e3b09d8fc0cb74b28c8623415ec2
helepod <gary.santos97@gmail.com>
2017-09-05 12:09:20 -0400

    Deleted gfycat, error handling in ImgurDownloader2.py

--- ImgurDownloader2.py
+++ ImgurDownloader2.py
@@ -1,46 +1,59 @@
 from imgurpython import ImgurClient
+import imgurpython
 from creds import *
 import urllib.request
 import re
 import os
+import time
 
 def saveImage(link, author, sub, name, ext, direct):
     folder = direct + '\\' + sub + '\\' + author + '\\'
     if not os.path.exists(folder):
         os.makedirs(folder)
-    urllib.request.urlretrieve(link, folder + name + ext)
-
+    try:
+        urllib.request.urlretrieve(link, folder + name + ext)
+    except urllib.error.URLError:
+        time.sleep(20)
+        try:
+            urllib.request.urlretrieve(link, folder + name + ext)
+        except:
+            pass
 def saveAlbum(album, author, sub, sub_title, direct):
     counter = 0
     client = ImgurClient(Im_client_id, Im_client_secret) #Imgur client
-    album_data = client.get_album(album) #Whole album
-    folderName = album_data.title #album title
+    try:
+        album_data = client.get_album(album) #Whole album
+        folderName = album_data.title #album title
 
-    if not folderName:
-        folderName = sub_title + " - " + str(album)
-        folderName = formatName(folderName)
-    else:
-        folderName = folderName.replace(' ', '_')
-        folderName = formatName(folderName)
-    print(album)
-    images = client.get_album_images(album)
-    print(album)
+        if not folderName:
+            folderName = sub_title + " - " + str(album)
+            folderName = formatName(folderName)
+        else:
+            folderName = folderName.replace(' ', '_')
+            folderName = formatName(folderName)
+        print(album)
+        images = client.get_album_images(album)
+        print(album)
 
-    for image in images:
-        #print(str(image.link))
-        #print(str(image.description))
+        for image in images:
+            #print(str(image.link))
+            #print(str(image.description))
+            print(image.type)
+            folder = direct + '\\' + sub + '\\' + author + '\\' + str(folderName) + '\\'
+            #folder  = re.sub('[?/|\:<>*"]', '', folder)
 
-        folder = direct + '\\' + sub + '\\' + author + '\\' + str(folderName) + '\\'
-        #folder  = re.sub('[?/|\:<>*"]', '', folder)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
 
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+            writeDescription(image.description, image.id, folder, counter)
 
-        writeDescription(image.description, image.id, folder, counter)
+            urllib.request.urlretrieve(image.link, folder + "(" + str(counter) + ") " + str(image.id) + str(image.type).replace('image/','.'))
 
-        urllib.request.urlretrieve(image.link, folder + "(" + str(counter) + ") " + str(image.id) + str(image.type).replace('image/','.'))
-
-        counter = counter + 1
+            counter = counter + 1
+    except imgurpython.helpers.error.ImgurClientError:
+        with open(direct + '\\error.txt', 'a') as logFile:
+            logFile.write('ImgurClientError: ' + album + '\n')
+            logFile.close()
 
 def writeDescription(description, imageId, folder, counter):
     name = folder + "(" + str(counter) + ") " + imageId + '.txt'
@@ -49,8 +62,7 @@ def writeDescription(description, imageId, folder, counter):
     file.write('%s\n' % description)
 
 def formatName(title):
-    title = re.sub('[?/|\:<>*"]', '', title)
+    title = re.sub('[?/|\\\:<>*"]', '', title)
     if len(title) > 211:
         title = title[:210]
     return title
-	
