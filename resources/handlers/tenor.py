6d3115dafb6f9ca43eee566f9246a198083466d6
Lame Lemon <garybsantos@gmail.com>
2019-12-28 17:28:37 +0300

    Added custom save for Tenor links

--- resources/handlers/tenor.py
+++ resources/handlers/tenor.py
@@ -1,13 +1,12 @@
-import re
-import json
 import os
 import logging
 
 from .common import Common
 
+
 class Tenor(Common):
     valid_url = r'https?://tenor\.com/view/(?P<name>[\w-]+)-(?P<id>(\w)+$)'
-    
+
     def __init__(self, link, name, direct):
         self.logger = logging.getLogger(__name__)
 
@@ -18,14 +17,19 @@ class Tenor(Common):
         self.logger.debug("Saving {}".format(self.link))
         self.link = self.get_data()
         if self.link:
-            super().save()
+            self.direct = os.path.join(self.direct, self.format_name(self.name) + '.mp4')
+
+            self.logger.debug("Saving {} with extension {}".format(self.link, '.mp4'))
+
+            if not self.save_image():
+                return False
             return True
         else:
             return False
 
     def sanitize_url(self):
         self.link = self.link.replace("http:", "https:")
-    
+
     def get_data(self):
         page_html = self.get_html()
         if page_html:
