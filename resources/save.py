da178573f0d814bbca3128a1db3fdffc4dfb7d28
LameLemon <garybsantos@gmail.com>
2020-01-26 17:13:56 +0000

    Save class is not a singleton object

--- resources/save.py
+++ resources/save.py
@@ -2,8 +2,19 @@ import os
 import sys
 import logging
 
+class Singleton(type):
+    def __init__(self, *args, **kwargs):
+        self.__instance = None
+        super().__init__(*args, **kwargs)
 
-class Save:
+    def __call__(self, *args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__(*args, **kwargs)
+            return self.__instance
+        else:
+            return self.__instance
+
+class Save(metaclass=Singleton):
     def __init__(self, base_dir, template):
         self.logger = logging.getLogger(__name__)
         self.base_dir = base_dir
